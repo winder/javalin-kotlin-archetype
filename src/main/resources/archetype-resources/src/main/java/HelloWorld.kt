5b12b5a4551cdb63eb0ed97f637c7d659203d64f
winder <wwinder.unh@gmail.com>
2018-04-23 12:16:34 -0400

    Initial commit.

---
+++ src/main/resources/archetype-resources/src/main/java/HelloWorld.kt
@@ -0,0 +1,8 @@
+package com.willwinder.javalin;
+
+import io.javalin.Javalin
+
+fun main(args: Array<String>) {
+    val app = Javalin.start(7000)
+    app.get("/") { ctx -> ctx.result("Hello World") }
+}
